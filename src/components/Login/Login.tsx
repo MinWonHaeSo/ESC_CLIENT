c52094612a0ba320750462fd506668a4e0e1a16f
kyle kwon <kylekwon96@gmail.com>
2022-11-25 02:29:46 +0900

    Feat : login 레이아웃 완성 (#13)
    * Feat : Login 컴포넌트 분리 및 레이아웃 작업
    
    * Chore : import 경로 오탈자 수정
    
    * Fix: css 제거 오류 수정
    
    * Fix : Login 폴더Typo 변경
    
    * Fix : login -> Login directory
    
    * Fix : 로그인 브랜치 대소문자 변경
    
    * Fix : login 디렉토리 삭제
    
    * Fix : 불필요한 디렉토리 제거
    
    * Feat : Login 레이아웃 완성

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,106 @@
+import styled from '@emotion/styled';
+import { ReactComponent as NavbarLogo } from '@/assets/esc-logo.svg';
+import { Link } from 'react-router-dom';
+import palette from '@/lib/styles/palette';
+import LoginForm from './LoginForm';
+import SocialLogin from './SocialLogin';
+
+interface LoginProps {}
+
+const Login = (props: LoginProps) => {
+  console.log('');
+  return (
+    <LoginBlock>
+      <LoginHeader>
+        <Link to="/">
+          <NavbarLogo width="80px" height="80px" />
+        </Link>
+      </LoginHeader>
+      <LoginSection>
+        <Title>Login</Title>
+        <LoginForm />
+        <SocialLogin />
+        <RegisterWrapper>
+          <Li>
+            <Link to="/">회원가입</Link>
+          </Li>
+          <Li>
+            <span></span>
+          </Li>
+          <Li>
+            <Link to="/">비밀번호 찾기</Link>
+          </Li>
+        </RegisterWrapper>
+      </LoginSection>
+    </LoginBlock>
+  );
+};
+
+export default Login;
+
+const LoginBlock = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const LoginHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 3rem;
+  border-bottom: 1px solid ${palette.grey[100]};
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.125);
+  background-color: #fff;
+
+  a {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const LoginSection = styled.section`
+  padding: 0.5rem 3rem;
+`;
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.75rem;
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+const RegisterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+`;
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  height: 1rem;
+  font-size: 15px;
+  font-weight: 500;
+  color: ${palette.black[100]};
+
+  a {
+    padding: 0.75rem 1rem;
+  }
+
+  a:hover {
+    color: ${palette.pointColor.point};
+  }
+
+  a:active {
+    color: ${palette.pointColor.point};
+  }
+
+  span {
+    width: 1px;
+    height: 100%;
+    background-color: ${palette.black[100]};
+  }
+`;
