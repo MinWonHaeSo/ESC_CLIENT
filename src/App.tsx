196c61fdf2be74640e122c63759b49502e90535a
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-11-24 14:41:43 +0900

    Feat: 프로젝트 절대 경로 설정 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Homepage from './pages/Homepage';
 import { store } from './store/store';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
