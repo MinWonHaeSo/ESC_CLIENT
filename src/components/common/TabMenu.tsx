ec196447eb0be2d30647fa24702f77677f31a4b7
olhkyle <kylekwon96@gmail.com>
2022-11-28 23:08:15 +0900

    Feat : tabMenu 상수 분리

--- src/components/common/TabMenu.tsx
+++ src/components/common/TabMenu.tsx
@@ -1,23 +1,12 @@
 import { useState } from 'react';
 import palette from '@/lib/styles/palette';
 import styled from '@emotion/styled';
+import { tabMenus } from '@/constants/tabMenu';
 
 interface TabMenuProps {}
 
 const TabMenu = (props: TabMenuProps) => {
   const [tabId, setTabId] = useState<number>(0);
-  const tabMenus = [
-    {
-      id: 0,
-      name: '일반 사용자',
-      title: 'user',
-    },
-    {
-      id: 1,
-      name: '판매자',
-      title: 'manager',
-    },
-  ];
   return (
     <TabMenuBlock>
       {tabMenus.map(tab => (
---
+++ src/constants/tabMenu.ts
@@ -0,0 +1,12 @@
+export const tabMenus = [
+  {
+    id: 0,
+    name: '일반 사용자',
+    title: 'user',
+  },
+  {
+    id: 1,
+    name: '판매자',
+    title: 'manager',
+  },
+];
