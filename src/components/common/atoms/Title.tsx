720327c177a7cf1700b880a64eb93b1fa0fea1f8
olhkyle <kylekwon96@gmail.com>
2022-11-29 17:27:23 +0900

    Style : Title 컴포넌트 스타일링을 위한 props 타입 변경

--- src/components/common/atoms/Title.tsx
+++ src/components/common/atoms/Title.tsx
@@ -1,19 +1,30 @@
-import { typo } from '@/lib/styles/typo';
 import styled from '@emotion/styled';
 
 interface TitleProps {
   children: React.ReactNode;
   fontSize: string;
+  marginLeft?: string;
+  marginTop?: string;
 }
 
-const Title = ({ children, fontSize }: TitleProps) => {
-  return <STitle fontSize={fontSize}>{children}</STitle>;
+const Title = ({ children, fontSize, marginLeft, marginTop }: TitleProps) => {
+  return (
+    <STitle fontSize={fontSize} left={marginLeft} top={marginTop}>
+      {children}
+    </STitle>
+  );
 };
 
 export default Title;
 
-const STitle = styled.h1<{ fontSize: string }>`
-  margin-top: 20px;
-  margin-left: 20px;
-  ${({ fontSize }) => fontSize && `font-size: ${fontSize}`}
+type STitleProps = {
+  fontSize: string;
+  left: string | undefined;
+  top: string | undefined;
+};
+
+const STitle = styled.h1<STitleProps>`
+  ${({ top }) => top && `margin-top: ${top}`};
+  ${({ left }) => left && `margin-left: ${left}`};
+  ${({ fontSize }) => fontSize && `font-size: ${fontSize}`};
 `;
