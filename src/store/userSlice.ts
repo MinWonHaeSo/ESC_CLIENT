0a5c980cf7a3258b61dfdb2cfc92f67299de9f5b
olhkyle <kylekwon96@gmail.com>
2022-11-30 04:08:10 +0900

    Feat :  userSlice 초기 상태값 지정 및 action 추가

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,19 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+interface UserState {
+  userId: number;
+  email?: string;
+  nickname?: string;
+  image?: string;
+  loggedIn: boolean;
+}
+
+const initialState: UserState = {
   userId: 0,
+  loggedIn: false,
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    changeUser: (state, action) => {
+    changeUser: (state, action: PayloadAction<number>) => {
       state.userId = action.payload;
+    },
+    checkLoggedIn: (state, action: PayloadAction<boolean>) => {
+      state.loggedIn = action.payload;
     },
   },
 });
 
-export const { changeUser } = userSlice.actions;
+export const { changeUser, checkLoggedIn } = userSlice.actions;
 
 export const userReducer = userSlice.reducer;
