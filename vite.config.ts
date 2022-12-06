4774a175f53458169f9840e8e2b123321512f74a
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-06 17:37:32 +0900

    Chore : index.html key 값 환경 변수로 관리

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    svgr(),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve('src') }],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://jsonplaceholder.typicode.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-        secure: false,
-        ws: true,
-      },
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            kakaoMapKey: env.VITE_KAKAO_KEY,
+            fontaesomeKey: env.VITE_FONTAWESOME_KEY,
+          },
+        },
+      }),
+      svgr(),
+    ],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve('src') }],
     },
-  },
-});
+  };
+};
