a83b38a4b1e140f809c39b5ac9c3e96b372deb26
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-06 17:59:16 +0900

    Feat : 체육관 업로드 RTK Slice 연동 작업 완료 (#31)
    * Feat : 체육관 업로드 하위 컴포넌트 RTK Slice 연동 작업

--- src/components/Stardium/StardiumEditElements/StardiumEditInput.tsx
+++ src/components/Stardium/StardiumEditElements/StardiumEditInput.tsx
@@ -1,20 +1,51 @@
 import Input from '@/components/common/atoms/Input';
-import React from 'react';
+import Label from '@/components/common/atoms/Label';
+import FormInputDivisionBlock from '@/components/common/Responsive/FormInputDivisionBlock';
+import { changeFiled } from '@/store/stardiumWriteSlice';
+import styled from '@emotion/styled';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 
-type StardiumEditInputProps = {
+interface StardiumEditInputProps {
+  value: string | number;
+  name: string
   title: string;
   id: string;
   type: string;
   placeholder: string;
 };
 
-const StardiumEditInput = ({ title, id, type, placeholder }: StardiumEditInputProps) => {
+const StardiumEditInput = ({ value, name, title, id, type, placeholder }: StardiumEditInputProps) => {
+  const dispatch = useDispatch();
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      dispatch(changeFiled({ name, value }));
+    },
+    [dispatch],
+  );
+
   return (
-    <div>
-      <label htmlFor="stardiumName">{title}</label>
-      <Input type={type} id={id} placeholder={placeholder} />
-    </div>
+    <EditInputWrapper>
+      <Label htmlFor={id} required={false}>
+        * {title}
+      </Label>
+      <Input
+        type={type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        required={true}
+        value={value}
+        onChange={handleChange}
+      />
+    </EditInputWrapper>
   );
 };
 
-export default StardiumEditInput;
+const EditInputWrapper = styled.div`
+  ${FormInputDivisionBlock};
+`;
+
+export default React.memo(StardiumEditInput);
