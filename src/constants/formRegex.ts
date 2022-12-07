afa626a78808993a47a6929e422478a1f997bf41
olhkyle <kylekwon96@gmail.com>
2022-12-07 19:54:14 +0900

    Refactor : password 오탈자 수정 및 Password 컴포넌트 리팩토링

--- src/constants/formRegex.ts
+++ src/constants/formRegex.ts
@@ -1,7 +1,7 @@
 const emailRegex = /^[-0-9A-Za-z!#$%&'*+/=?^_`{|}~.]+@[-0-9A-Za-z!#$%&'*+/=?^_`{|}~]+[.]{1}[0-9A-Za-z]/;
 
-const passWordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
 
-const formRegex = { emailRegex, passWordRegex };
+const formRegex = { emailRegex, passwordRegex };
 
 export default formRegex;
