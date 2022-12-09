2261d28faeee1e4f847374d6927054b9080bc5d4
olhkyle <kylekwon96@gmail.com>
2022-12-09 20:53:16 +0900

    Feat : 소셜 로그인 유지 처리 및 accessToken 관리 로직 수정

--- src/components/OAuth/OAuthRedirect.tsx
+++ src/components/OAuth/OAuthRedirect.tsx
@@ -1,7 +1,6 @@
 import { useGetSocialLoginQuery } from '@/api/authApi';
-
 import { setCookie } from '@/lib/utils/manageCookies';
-import { setCredentials } from '@/store/authSlice';
+import { checkLoggedIn, setCredentials } from '@/store/authSlice';
 import { useAppDispatch } from '@/store/store';
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router';
@@ -22,27 +21,38 @@ const OAuthRedirect = () => {
 
   const getAccessToken = async (social: string) => {
     // 인가코드
-    let accessToken = new URL(window.location.href).searchParams.get('token') as string;
+    const accessToken = new URL(window.location.href).searchParams.get('token') as string;
     console.log(accessToken);
-    try {
-      const { data, isLoading, error } = useGetSocialLoginQuery({ social: social, token: accessToken });
-      if (data) {
-        const { refreshToken } = data;
-        const decodedUserInfo = jwt_decode(accessToken);
-        setCookie('refreshToken', refreshToken, {
-          path: '/',
-          secure: true,
-          httpOnly: true,
-        });
-        dispatch(setCredentials({ user: decodedUserInfo, token: accessToken }));
-        // decode한 user의 이메일에 저장
-        // loggedin한 상태로 저장
-        // accessToken 만료 시 refresh token 보내어 어떻게 accessToken을 다시 받아올 지 생각
-      }
-    } catch {
-      console.error('전달받은 토큰이 없습니다.');
-      navigate('/login');
+    if (accessToken) {
+      const decodedUserInfo = jwt_decode(accessToken); // 이메일 정보 뽑아내기
+      dispatch(setCredentials({ user: decodedUserInfo, token: accessToken })); // 전역 상태에 user 정보, accessToken 정보 저장
+      dispatch(checkLoggedIn(true));
+      // refresh token은 baseQueryReauth 부분에서 accessToken이 만료되어 없을 경우 ,
+      // refreshToken이 브라우저 전역 상 set-cookie에 담겨 있으므로 굳이 저장하지 않아도되는지 확인해야함.
+      // setCookie('refreshToken', refreshToken, {
+      //   path: '/',
+      //   secure: true,
+      //   httpOnly: true,
+      // });
     }
+    // try {
+    //   const { data, isLoading, error } = useGetSocialLoginQuery({ social: social, token: accessToken });
+    //   if (data) {
+    //     const { refreshToken } = data;
+    //     const decodedUserInfo = jwt_decode(accessToken);
+    //     setCookie('refreshToken', refreshToken, {
+    //       path: '/',
+    //       secure: true,
+    //       httpOnly: true,
+    //     });
+    //     // decode한 user의 이메일에 저장
+    //     // loggedin한 상태로 저장
+    //     // accessToken 만료 시 refresh token 보내어 어떻게 accessToken을 다시 받아올 지 생각
+    //   }
+    // } catch {
+    //   console.error('전달받은 토큰이 없습니다.');
+    //   navigate('/login');
+    // }
 
     navigate('/');
   };
