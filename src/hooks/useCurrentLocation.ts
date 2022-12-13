798ba418622260357a54fa5391aa91fc446c1178
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-13 22:21:26 +0900

    Style : default 위치 constants 데이터로 관리

--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -1,31 +1,33 @@
+import ZERO_LOCATION from '@/constants/defaultLocation';
+import sw from '@/lib/utils/customSweetAlert';
 import { useState, useEffect } from 'react';
 
 export type geoLocationType = {
   lat: string;
   lnt: string;
-}
+};
 
 const useCurrentLocation = () => {
-  const [geoLocation, setGeoLocation] = useState<geoLocationType>({lat: '', lnt:''});
+  const [geoLocation, setGeoLocation] = useState<geoLocationType>({ lat: ZERO_LOCATION.lat, lnt: ZERO_LOCATION.lnt });
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        console.log(`lat : ${position.coords.latitude} lnt: ${position.coords.longitude}`);
-        setGeoLocation({ lat: String(latitude), lnt: String(longitude) });
-      },
-        (error) => {
-          console.log(error);
-      })
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setGeoLocation({ lat: String(latitude), lnt: String(longitude) });
+        },
+        error => {
+          console.error(`geoLocation 에러 : ${error}`);
+          sw.toast.error('다시 시도해 주세요.');
+        },
+      );
     } else {
-      console.log('GPS를 지원하지 않습니다.');
+      sw.toast.error('GPS를 지원하지 않습니다.');
     }
-  }, [])
-  
-  return geoLocation
+  }, []);
+
+  return geoLocation;
 };
 
 export default useCurrentLocation;
-
-
