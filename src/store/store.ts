a94159c5c04573579e099f02554011c5ac2f830c
olhkyle <kylekwon96@gmail.com>
2022-12-18 04:17:20 +0900

    Feat : 알림 내역 페이지 및 컴포넌트의 추가로 해당 전역 상태 관리 reducer store에 연결

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { baseApi } from '@/api/baseApi';
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import { authReducer } from './authSlice';
+import { notificationReducer } from './notificationSlice';
 import { searchPasswordReducer } from './searchPassWordSlice';
 import { stardiumReducer } from './stardiumSlice';
 import { stardiumWriteReducer } from './stardiumWriteSlice';
@@ -14,6 +15,7 @@ export const store = configureStore({
     stardiumWrite: stardiumWriteReducer,
     searchPassword: searchPasswordReducer,
     auth: authReducer,
+    notification: notificationReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
