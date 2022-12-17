919dea46a652796047d7d68d320a01e5dfe0c8f4
olhkyle <kylekwon96@gmail.com>
2022-12-18 04:04:21 +0900

    Feat : Footer 컴포넌트 추가 후 Layout 컴포넌트에 연결

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,9 +1,14 @@
+import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
+import media from '@/lib/styles/media';
 import palette from '@/lib/styles/palette';
+import { RootState } from '@/store/store';
 import styled from '@emotion/styled';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  const userType = useSelector((state: RootState) => state.user.type);
   return (
     <>
       <HeaderWrapper>
@@ -12,6 +17,9 @@ const Layout = () => {
       <MainWrapper>
         <Outlet />
       </MainWrapper>
+      <FooterWrapper userType={userType}>
+        <Footer />
+      </FooterWrapper>
     </>
   );
 };
@@ -30,4 +38,12 @@ const MainWrapper = styled.main`
   padding-top: 5rem;
 `;
 
+const FooterWrapper = styled.footer<{ userType: 'USER' | 'MANAGER' }>`
+  margin-top: 10rem;
+
+  ${media.smallMin} {
+    margin-top: 18rem;
+    ${({ userType }) => userType === 'MANAGER' && `margin-top: 25rem`};
+  }
+`;
 export default Layout;
