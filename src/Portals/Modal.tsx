e192ebe3c2efcf278a06ec69f58168e0df5c2521
olhkyle <kylekwon96@gmail.com>
2022-12-20 00:54:32 +0900

    Refactor : Modal 컴포넌트 리팩토링

--- src/Portals/Modal.tsx
+++ src/Portals/Modal.tsx
@@ -13,18 +13,19 @@ interface Modal {
 
 const Modal = ({ id, isOpen, onClose }: Modal) => {
   useEffect(() => {
-    // 위치 고정
-    document.body.style.cssText = `
-          position: fixed; 
-          top: -${window.scrollY}px;
-          overflow-y: scroll;
-          width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
-  }, []);
+    if (isOpen) {
+      document.body.style.cssText = `
+            position: fixed; 
+            top: -${window.scrollY}px;
+            overflow-y: scroll;
+            width: 100%;`;
+      return () => {
+        const scrollY = document.body.style.top;
+        document.body.style.cssText = '';
+        window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+      };
+    }
+  }, [isOpen]);
 
   return (
     <Portal id={id}>
