2aa4376399caf2badad96d564f3cfd669b040e66
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-21 15:35:23 +0900

    Fix : remove error

--- src/hooks/useCurrentLocation.ts
+++ src/hooks/useCurrentLocation.ts
@@ -9,19 +9,14 @@ export type geoLocationType = {
 
 const useCurrentLocation = () => {
   const [geoLocation, setGeoLocation] = useState<geoLocationType>({ lat: ZERO_LOCATION.lat, lnt: ZERO_LOCATION.lnt });
+  // 위치 서비스 기본값 ZERO BASE 주소
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          const { latitude, longitude } = position.coords;
-          setGeoLocation({ lat: String(latitude), lnt: String(longitude) });
-        },
-        error => {
-          console.error(`geoLocation 에러 : ${error}`);
-          sw.toast.error('다시 시도해 주세요.');
-        },
-      );
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setGeoLocation({ lat: String(latitude), lnt: String(longitude) });
+      });
     } else {
       sw.toast.error('GPS를 지원하지 않습니다.');
     }
