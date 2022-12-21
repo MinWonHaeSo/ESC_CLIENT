509b487819152ba61ef475fdc136229403117039
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-22 01:12:49 +0900

    Fix : ReservationPage Stadium Id Router State 관리

--- src/pages/StadiumReservationPage.tsx
+++ src/pages/StadiumReservationPage.tsx
@@ -1,8 +1,24 @@
-import Reservation from '@/components/StadiumReservation/Reservation';
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import sw from '@/lib/utils/customSweetAlert';
+import Reservation from '@/components/StadiumReservation/Reservation';
+
+type MyLocationState = {
+  id: string;
+};
 
 const StadiumReservationPage = () => {
-  return <Reservation />;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const state = location.state as MyLocationState;
+
+  if (!state.id) {
+    sw.toast.error('잘못된 경로 요청입니다.');
+    navigate(-1);
+  }
+
+  return <Reservation id={state.id} />;
 };
 
 export default StadiumReservationPage;
