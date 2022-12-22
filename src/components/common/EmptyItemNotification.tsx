18b7b15eed047ddef388c3b651060167180a03b6
olhkyle <kylekwon96@gmail.com>
2022-12-22 12:13:18 +0900

    Feat : 예약 목록, 찜한 체육관, 최근 본체육관이 비어있을 때 보여줄 빈아이템알림 메세지 컴포넌트 추가

---
+++ src/components/common/EmptyItemNotification.tsx
@@ -0,0 +1,55 @@
+import { DEFAULT_ICONURL } from '@/constants/defaultImage';
+import PATH from '@/constants/path';
+import palette from '@/lib/styles/palette';
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router';
+import Button from './atoms/Button';
+
+interface EmptyItemProps {
+  message: string;
+}
+
+const EmptyItemNotification = ({ message }: EmptyItemProps) => {
+  const navigate = useNavigate();
+  return (
+    <EmptyItemNotificationBlock>
+      <img src={DEFAULT_ICONURL} alt="스마일" />
+      <EmptyDesc>{message}</EmptyDesc>
+      <Button
+        type={'button'}
+        size={'large'}
+        backgroundColor={`${palette.primary['green']}`}
+        onClick={() => navigate(`${PATH.ROOT}`)}
+      >
+        체육관 보러가기
+        <ArrowIcon className="fa-solid fa-location-arrow" />
+      </Button>
+    </EmptyItemNotificationBlock>
+  );
+};
+
+export default EmptyItemNotification;
+
+const EmptyItemNotificationBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  height: 300px;
+
+  img {
+    display: inline-block;
+    margin: 20px auto;
+    width: 90px;
+  }
+`;
+
+const EmptyDesc = styled.p`
+  color: ${palette.grey[500]};
+  font-weight: 400;
+`;
+
+const ArrowIcon = styled.i`
+  margin-left: 8px;
+`;
