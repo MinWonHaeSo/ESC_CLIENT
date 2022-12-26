c8ba631f9e13c452aeef8e3271f405bf0f9a5f20
olhkyle <kylekwon96@gmail.com>
2022-12-27 04:01:20 +0900

    Feat : 예약내역, 찜한 체육관 내역 페이지네이션을 위한  useLazyQuery 사용으로 extraReducer 추가

--- src/store/pagingSlice.ts
+++ src/store/pagingSlice.ts
@@ -1,5 +1,5 @@
 import { ManagerListStadium, RentalStadium, stadiumApi } from '@/api/stadiumApi';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface pagingState {
   content: RentalStadium[];
@@ -18,13 +18,31 @@ const initialState: pagingState = {
 };
 
 export const pagingSlice = createSlice({
-  name: 'stadiumReview',
+  name: 'paging',
   initialState,
   reducers: {
+    toggleLike: (state, action: PayloadAction<{ id: string; value: boolean }>) => {
+      // const findIdx = state.content.findIndex(item => item.);
+    },
     clearPaging: () => initialState,
   },
   extraReducers: builder => {
     builder.addMatcher(stadiumApi.endpoints.getStadiumManagerList.matchFulfilled, (state, { payload }) => {
+      state.content = [...state.content, ...payload.content];
+      state.currentPage = payload.number;
+      state.nextPage = payload.number + 1;
+      state.totalPage = payload.totalPages;
+      state.isLast = payload.last;
+    });
+    builder.addMatcher(stadiumApi.endpoints.getRentalStadiumList.matchFulfilled, (state, { payload }) => {
+      state.content = [...state.content, ...payload.content];
+      state.currentPage = payload.number;
+      state.nextPage = payload.number + 1;
+      state.totalPage = payload.totalPages;
+      state.isLast = payload.last;
+    });
+    builder.addMatcher(stadiumApi.endpoints.getLikeStadiumList.matchFulfilled, (state, { payload }) => {
+      console.log(payload);
       state.content = [...state.content, ...payload.content];
       state.currentPage = payload.number;
       state.nextPage = payload.number + 1;
