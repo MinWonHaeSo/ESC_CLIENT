2556dacdc078a2fb8c4c9ec46605c1fb95d06578
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-12-27 16:28:06 +0900

    Refactor : extraReducer dispatch abstract

--- src/store/pagingSlice.ts
+++ src/store/pagingSlice.ts
@@ -1,4 +1,4 @@
-import { ManagerListStadium, RentalStadium, stadiumApi } from '@/api/stadiumApi';
+import { ManagerListStadium, RentalStadium, ReservationStatus, stadiumApi } from '@/api/stadiumApi';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface pagingState {
@@ -7,7 +7,16 @@ interface pagingState {
   nextPage: number;
   totalPage: number;
   isLast: boolean;
+  type: string;
 }
+
+const pagingDispatch = (state: pagingState, payload: any) => {
+  state.content = [...state.content, ...payload.content];
+  state.currentPage = payload.number;
+  state.nextPage = payload.number + 1;
+  state.totalPage = payload.totalPages;
+  state.isLast = payload.last;
+};
 
 const initialState: pagingState = {
   content: [],
@@ -15,12 +24,17 @@ const initialState: pagingState = {
   nextPage: 0,
   totalPage: 0,
   isLast: false,
+  type: '',
 };
 
 export const pagingSlice = createSlice({
   name: 'paging',
   initialState,
   reducers: {
+    changeStatus: (state, action: PayloadAction<{ id: string; status: ReservationStatus }>) => {
+      const findIdx = state.content.findIndex(item => item.reservationId === action.payload.id);
+      state.content[findIdx].status = action.payload.status;
+    },
     toggleLike: (state, action: PayloadAction<{ id: string; value: boolean }>) => {
       // const findIdx = state.content.findIndex(item => item.);
     },
@@ -28,30 +42,28 @@ export const pagingSlice = createSlice({
   },
   extraReducers: builder => {
     builder.addMatcher(stadiumApi.endpoints.getStadiumManagerList.matchFulfilled, (state, { payload }) => {
-      state.content = [...state.content, ...payload.content];
-      state.currentPage = payload.number;
-      state.nextPage = payload.number + 1;
-      state.totalPage = payload.totalPages;
-      state.isLast = payload.last;
+      pagingDispatch(state, payload);
+      state.type = 'managerStadiumList';
     });
     builder.addMatcher(stadiumApi.endpoints.getRentalStadiumList.matchFulfilled, (state, { payload }) => {
-      state.content = [...state.content, ...payload.content];
-      state.currentPage = payload.number;
-      state.nextPage = payload.number + 1;
-      state.totalPage = payload.totalPages;
-      state.isLast = payload.last;
+      pagingDispatch(state, payload);
+      state.type = 'rentalStadiumList';
+    });
+    builder.addMatcher(stadiumApi.endpoints.getMoreRentalStadiumList.matchFulfilled, (state, { payload }) => {
+      pagingDispatch(state, payload);
+      state.type = 'rentalStadiumList';
     });
     builder.addMatcher(stadiumApi.endpoints.getLikeStadiumList.matchFulfilled, (state, { payload }) => {
-      console.log(payload);
-      state.content = [...state.content, ...payload.content];
-      state.currentPage = payload.number;
-      state.nextPage = payload.number + 1;
-      state.totalPage = payload.totalPages;
-      state.isLast = payload.last;
+      pagingDispatch(state, payload);
+      state.type = 'likeStadiumList';
+    });
+    builder.addMatcher(stadiumApi.endpoints.getMoreLikeStadiumList.matchFulfilled, (state, { payload }) => {
+      pagingDispatch(state, payload);
+      state.type = 'likeStadiumList';
     });
   },
 });
 
-export const { clearPaging } = pagingSlice.actions;
+export const { changeStatus, clearPaging } = pagingSlice.actions;
 
 export const pagingReducer = pagingSlice.reducer;
